250e1a2ed4f5d44a163e3f8b9618741aef6b196e
clbertolini <carterbertolini@gmail.com>
2023-01-14 22:14:38 -0500

    Added clean up for image files after video has stopped being played.

--- include/gui.h
+++ include/gui.h
@@ -9,3 +9,14 @@
 int init_gui(std::string video_id, uint64_t frame_count, uint16_t framerate);
 
 #endif //INC_1_GUI_H
+//
+// Created by bertoc2 on 1/14/2023.
+//
+
+#ifndef INC_1_GUI_H
+#define INC_1_GUI_H
+#include <string>
+
+int init_gui(std::string video_id, uint64_t frame_count, uint16_t framerate);
+
+#endif //INC_1_GUI_H
